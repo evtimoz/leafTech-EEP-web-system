a79c73e7ffe02d689628bfd6c2c5f70b0047695b
laskaa <32509071+laskaa@users.noreply.github.com>
2020-04-10 01:17:23 +0300

    + For transforming retrieved Firebase document into Product object: - Access type changed - Getters + setters added - Empty public constructor added

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -4,11 +4,52 @@ package model;
  * Created by arman 4/9/20.
  */
 public class Product {
-    public String Id;
-    public String Type;
-    public String Description;
-    public double Price;
-    public int Quantity;
+    private String Id;
+    private String Type;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public double getPrice() {
+        return Price;
+    }
+
+    public void setPrice(double price) {
+        Price = price;
+    }
+
+    public int getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        Quantity = quantity;
+    }
+
+    private String Description;
+    private double Price;
+    private int Quantity;
 
     public Product(String id, String type, String description, double price, int quantity) {
         Id = id;
@@ -17,4 +58,6 @@ public class Product {
         Price = price;
         Quantity = quantity;
     }
+
+    public Product(){}
 }
