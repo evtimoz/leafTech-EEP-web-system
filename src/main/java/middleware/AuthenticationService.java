d08fa7d9ef03d6e484905e042d1ea49cf18112d9
Evgenii <zheny.tim@gmail.com>
2020-04-12 16:59:47 +0300

    + Encryption

--- src/main/java/middleware/AuthenticationService.java
+++ src/main/java/middleware/AuthenticationService.java
@@ -1,10 +1,18 @@
 package middleware;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLOutput;
+
 /**
  * Created by arman 4/9/20.
  */
 public class AuthenticationService {
     private static AuthenticationService instance;
+    private static final String HASH_METHOD = "SHA-256";
+    private static final String CHARSET = "UTF-8";
+    private static final int DEFAULT_BYTECOUNT = 32;
 
     private AuthenticationService(){ }
 
@@ -24,11 +32,28 @@ public class AuthenticationService {
         FirebaseGateway firebaseGateway = new FirebaseGateway();
         firebaseGateway.LogEvent("login", login);
 
-        return firebaseGateway.GetUserRoleByLoginAndPassword(login, password);
+        String hashedPassword = GenerateHash(password);
+        System.out.println(hashedPassword);
+        return firebaseGateway.GetUserRoleByLoginAndPassword(login, hashedPassword);
     }
 
     public void LogEvent(String type, String userName) throws Exception{
         FirebaseGateway firebaseGateway = new FirebaseGateway();
         firebaseGateway.LogEvent(type, userName);
+    }
+
+    private String GenerateHash(String input) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+
+        MessageDigest digest = MessageDigest.getInstance(AuthenticationService.HASH_METHOD);
+
+        // Convert input strings to UTF-8 encoded byte arrays
+        byte[] inputBytes = input.getBytes(AuthenticationService.CHARSET);
+
+        digest.reset();
+
+        byte[] outputBytes = digest.digest(inputBytes);
+        String outputString = Encryption.encodeBytes(outputBytes);
+
+        return outputString;
     }
 }
